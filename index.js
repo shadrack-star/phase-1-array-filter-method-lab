2881dd92fd77dcab420b30dcd73a245effd67639
shadrack-star <shaddkiplangat98@gmail.com>
2024-03-26 13:59:51 +0300

    array filter method

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+const driver=[{driver_name:"Felix"
+,home_town:"Nairobi"}, {driver_name:"Hesbon",home_town:"Thika"}]
+function findMatching(source, sought) {
+    return source.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+    );
+    } 
+    function fuzzyMatch(source, testString) {
+        return source.filter(
+          (possibleMatch) =>
+            possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+        );
+      }
+      function matchName(source, soughtName) {
+        return source.filter((record) => record.name === soughtName);
+      }
